d1fc76d552d8a8cd9b8510b5583588608be19a29
sairamya27 <sairamyakothapalli@gmail.com>
2020-05-29 14:28:23 -0700

    Event Detail Activity Reg/DReg

--- app/src/main/java/com/project/scuevents/EventDetailActivity.java
+++ app/src/main/java/com/project/scuevents/EventDetailActivity.java
@@ -58,13 +58,13 @@ public class EventDetailActivity extends AppCompatActivity{
         group = (EventClass) i.getSerializableExtra("Object");
 
         SharedPreferences sh = getSharedPreferences("USER_TOKENS", MODE_PRIVATE);
-        final String uId = sh.getString("USER_ID", "");
-        Log.d(TAG, "onCreate: "+uId);
+        final String userId = sh.getString("USER_ID", "");
+        Log.d(TAG, "onCreate: "+userId);
         db = FirebaseDatabase.getInstance().getReference();
         Query query = db.child("Events").child(group.getEventID()).child("registeredUsers");
 
         //DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
-        Query q=db.child("Events").child(group.getEventID()).child("registeredUsers").child(uId);
+        Query q=db.child("Events").child(group.getEventID()).child("registeredUsers").child(userId);
         Log.d(TAG, "onCreateUID: "+q);
         q.addValueEventListener(new ValueEventListener() {
            @Override
@@ -124,16 +124,13 @@ public class EventDetailActivity extends AppCompatActivity{
     private void deregisterUsers(View view) {
         db = FirebaseDatabase.getInstance().getReference();
         SharedPreferences sh = getSharedPreferences("USER_TOKENS", MODE_PRIVATE);
-        final String uId = sh.getString("USER_ID", "");
-        Query delQuery=db.child("Events").child(group.getEventID()).child("registeredUsers").child(uId);
+        final String userId = sh.getString("USER_ID", "");
+        Query delQuery=db.child("Events").child(group.getEventID()).child("registeredUsers").child(userId);
         delQuery.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                for (DataSnapshot delSnapshot: dataSnapshot.getChildren()) {
-                    delSnapshot.getRef().removeValue();
+                    dataSnapshot.getRef().removeValue();
                     RegButton.setText("Register");
-                }
-
             }
             @Override
             public void onCancelled(DatabaseError databaseError) {
@@ -141,14 +138,11 @@ public class EventDetailActivity extends AppCompatActivity{
             }
         });
 
-        Query delEvent=db.child("Users").child(uId).child("registeredEvents").child(group.getEventID());
+        Query delEvent=db.child("Users").child(userId).child("registeredEvents").child(group.getEventID());
         delEvent.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                for (DataSnapshot delSnapshot: dataSnapshot.getChildren()) {
-                    delSnapshot.getRef().removeValue();
-                    //RegButton.setText("Register");
-                }
+                dataSnapshot.getRef().removeValue();
 
             }
             @Override
@@ -165,10 +159,9 @@ public class EventDetailActivity extends AppCompatActivity{
         String regID = FireBaseUtilClass.getDatabaseReference().child("Events").child("registeredUsers").push().getKey();
         SharedPreferences sh = getSharedPreferences("USER_TOKENS", MODE_PRIVATE);
         String userId = sh.getString("USER_ID", "");
-       // String dName = sh.getString("USER_NAME","");
         SharedPreferences pref = getSharedPreferences("MyPreferenceFileName", MODE_PRIVATE);
-        String uToken=pref.getString("UserToken","");
-        this.addUsersToFirebase(new UserDetails(userId,uToken));
+        String userToken=pref.getString("UserToken","");
+        this.addUsersToFirebase(new UserDetails(userId,userToken));
     }
     private void addUsersToFirebase(final UserDetails item) {
         FireBaseUtilClass.getDatabaseReference().child("Events").child(group.getEventID()).child("registeredUsers").child(item.getUserID()).setValue(item).addOnSuccessListener(new OnSuccessListener<Void>() {
