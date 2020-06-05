68e9bac10ca6b87f50b2ee7658d9150ce54c9be3
PoojaGhosh2019 <pooja.sun@gmail.com>
2020-06-04 19:21:48 -0700

    RegisteredEventsUpdate+notificationBadgeUpdate

---
+++ app/src/main/java/com/project/scuevents/ui/registeredevent/RegisteredEventsFragment1.java
@@ -0,0 +1,50 @@
+package com.project.scuevents.ui.registeredevent;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.android.material.tabs.TabLayout;
+import com.project.scuevents.R;
+import com.project.scuevents.adapter.RegisteredChildSelectionAdapter;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class RegisteredEventsFragment1 extends Fragment {
+    View root;
+    ViewPager viewPager;
+    TabLayout tabLayout;
+    public RegisteredEventsFragment1() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        root = inflater.inflate(R.layout.fragment_registered_events1, container, false);
+
+        viewPager = root.findViewById(R.id.viewPager);
+        tabLayout = root.findViewById(R.id.tabLayout);
+        return root;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        RegisteredChildSelectionAdapter adapter= new RegisteredChildSelectionAdapter(getChildFragmentManager());
+        adapter.addFragment(new UpcomingEventFragment(),"Upcoming Events");
+        adapter.addFragment(new PastEventFragment(),"Past Events");
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+}
